9b7db1c462ba689dbe12e1be4a2fb5c5b310ee2e
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2024-07-04 19:52:22 +0530

    feat: add tmux config

--- nvim/init.lua
+++ nvim/init.lua
@@ -647,17 +647,19 @@ require('lazy').setup {
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
+    'ellisonleao/gruvbox.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      local theme = require 'tokyonight'
+      local theme = require 'gruvbox'
       theme.setup {
-        transparent = true,
+        -- transparent_mode = true,
+        underline = false,
+        bold = false,
       }
 
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'tokyonight'
+      vim.cmd.colorscheme 'gruvbox'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
